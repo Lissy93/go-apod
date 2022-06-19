bad777696022d9f5efe141d77610d74549328e30
Alicia Sykes <alicia@omg.lol>
2022-06-19 11:20:51 +0100

    📼 Adds support for video content, and add loader

--- static/script.js
+++ static/script.js
@@ -1,5 +1,6 @@
 
 
+/* Fetch data from APOD API */
 const makeRequest = () => {
   const apiUrl = 'https://go-apod.herokuapp.com/apod';
 
@@ -7,29 +8,51 @@ const makeRequest = () => {
   .then(response => response.json())
   .then(data => {
     console.log(data);
-    updateDom(data)
+    hideLoader();
+    updateDom(data);
   })
   .catch((error) => {
     console.error('Error:', error);
   });
 }
 
+const hideLoader = () => {
+  document.getElementById('loader').style.display = 'none';
+  document.getElementsByClassName('apod-info')[0].style.display = 'block';
+};
+
+const formatDate = (date) => {
+  if (!date) return '';
+  return new Date().toLocaleDateString(
+    "en-US",
+    { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' },
+  );
+};
+
+/* Using the response from APOD API, update the DOM to render results */
 const updateDom = (apod) => {
   const titleElem = document.getElementById('apod-title');
   const descriptionElem = document.getElementById('apod-explanation');
   const copyrightElem = document.getElementById('apod-copyright');
   const dateElem = document.getElementById('apod-date');
   const linkElem = document.getElementById('apod-hd-link');
+  const iframeElem = document.getElementById('apod-dynamic-content');
+  const imageElem = document.getElementById('apod-picture');
 
   titleElem.innerText = apod.title;
   descriptionElem.innerText = apod.explanation;
-  copyrightElem.innerText = apod.copyright;
-  dateElem.innerText = apod.date;
+  copyrightElem.innerText = apod.copyright || '';
+  dateElem.innerText = formatDate(apod.date);
   linkElem.innerText = 'View HD Image';
-  linkElem.setAttribute('href', apod.hdurl);
+  linkElem.setAttribute('href', apod.hdurl || apod.url);
+
+  if (apod.media_type !== 'image') {
+    iframeElem.setAttribute('src', apod.url);
+    iframeElem.style.display = 'block';
+    imageElem.style.display = 'none';
+    linkElem.innerText = 'View Dynamic Content';
+  }
 }
 
-
-// console.log('Hello');
 
 makeRequest();
