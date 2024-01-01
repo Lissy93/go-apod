57d2282a3f40afdd6b9a8e45f36206e3a62eb97a
Alicia Sykes <alicia@omg.lol>
2024-01-01 13:02:12 +0000

    🔐 Adds support for custom origins

--- api/apod.go
+++ api/apod.go
@@ -3,11 +3,26 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"os"
 
 	"github.com/lissy93/go-apod/shared"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	corsAllowedOrigins := os.Getenv("CORS_ALLOWED_ORIGINS")
+	if corsAllowedOrigins == "" {
+		corsAllowedOrigins = "*" // Default to allowing all origins
+	}
+
+	// Set CORS headers
+	w.Header().Set("Access-Control-Allow-Origin", corsAllowedOrigins)
+	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	if r.Method == http.MethodOptions {
+		return
+	}
+
 	conf, err := shared.NewConfig()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
